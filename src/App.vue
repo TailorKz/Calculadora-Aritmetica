f84a2a3c853f4afa33efa03a3b51c3581d62dac3
MitsukiKz <tailorkunz@gmail.com>
2024-11-26 17:03:08 -0300

    iniciando o projeto

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1 id="h1" class="mt-5">Calculadora aritmética</h1>
+  <div class="grid text-center">
+
+  
+  <select class="form-select" aria-label="Default select example">
+    <option selected>Operador:</option>
+    <option value="1">+</option>
+    <option value="2">-</option>
+    <option value="3">x</option>
+    <option value="4">÷</option>
+  </select></div>
+</template>
+
+<style scoped>
+#h1 {
+  display: flex;
+  text-align: center;
+  margin: 0 auto;
+  justify-content: center;
+}
+</style>
